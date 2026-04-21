00ed3df135335ad89415a73ef29f07d5249ecf2e
Isaac244 <is.ascanoa@gmail.com>
2026-04-21 15:16:52 -0500

    Webhook funcionando 2

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ def recibir_mensajes(req):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
     #serve(app, host='0.0.0.0', port=80)
